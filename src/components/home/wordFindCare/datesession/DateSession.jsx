f601bbf209903ead71d13bc99bd0b4baf31c1e8f
ANHTUANPHUNG <132957644+ANHTUANPHUNG@users.noreply.github.com>
2024-01-11 09:31:05 +0700

    tuandev

--- src/components/home/wordFindCare/datesession/DateSession.jsx
+++ src/components/home/wordFindCare/datesession/DateSession.jsx
@@ -1,13 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import LogoProject from "../../../logoProject/LogoProject";
 import { LegalNotice } from "../../../carehub/LegalNotice";
-import { NavLink } from "react-router-dom";
+import {  useNavigate, useParams } from "react-router-dom";
 import "./DateSession.css";
 import { ButtonForMe } from "../../../ButtonForMe";
 import { SelectDate } from "../../../selectDate/SelectDate";
+import DateBetween from "./DateBetween";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export function DateSession() {
- 
+  const [value, setValue] = useState();
+  const [selectedDate, setSelectedDate] = useState(undefined);
+  const [dayInWeek, setDayInWeek] = useState([]);
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+  let navigate = useNavigate();
+  const { id } = useParams();
+  const getDaysInRange = (startDate, endDate) => {
+    const days = [];
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= endDate) {
+      days.push({
+        date: new Date(currentDate),
+        dayOfWeek: currentDate.toLocaleDateString("en-US", { weekday: "long" }), // Lấy tên thứ
+      });
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return days;
+  };
+  useEffect(() => {
+    if (selectedDate != null) {
+      const daysInRange = getDaysInRange(selectedDate[0], selectedDate[1]);
+      const dayInWeekSelect = daysInRange.map((e) => e.dayOfWeek);
+      setDayInWeek(dayInWeekSelect);
+    }
+  }, [selectedDate]);
+  const handleSubmitDate = async () => {
+    try {
+      if (Object.keys(value).length === 0) {
+        toast.error("Chọn ngày thuê");
+        return;
+      }
+
+      const transformedData = Object.keys(value).map((day) => ({
+        date: day,
+        sessionOfDateList: value[day],
+      }));
+
+      await axios.put(`http://localhost:8080/api/carts/dateSessions/${id}`, {
+        listDateSession: transformedData,
+      });
+
+      navigate(`/user/need-care/${id}`);
+      toast.success("Hoàn thành cập nhật ngày có thể làm");
+    } catch (error) {
+      console.error("Lỗi khi gửi PUT request:", error);
+      toast.error("Đã xảy ra lỗi. Vui lòng thử lại sau.");
+    }
+  };
   return (
     <>
       <div>
@@ -16,7 +70,7 @@ export function DateSession() {
         </div>
 
         <div className="row ">
-          <div className="col-9 header"></div>
+          <div className="col-8 header"></div>
         </div>
       </div>
       <div className="d-flex mt-5 date-session">
@@ -24,19 +78,27 @@ export function DateSession() {
           <h4>Bạn cần chăm sóc những ngày nào?</h4>
         </div>
       </div>
-      <div className="date-session-map" >
-      <SelectDate/>
+      <div className="date-session-map">
+        <div style={{ margin: "0px 400px" }}>
+          <DateBetween
+            onChange={handleDateChange}
+            setSelectedDate={setSelectedDate}
+            selectedDate={selectedDate}
+          />
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          {selectedDate && selectedDate[0] && selectedDate[1] && (
+            <SelectDate dayInWeek={dayInWeek} setValue={setValue} />
+          )}{" "}
+        </div>
       </div>
-      
+
       <div className="d-flex justify-content-center mt-4">
-        {/* Don't worry, you can always adjust these times later. */}
         Đừng lo lắng, bạn luôn có thể chỉnh sửa nó sau.
       </div>
 
       <div className="mt-2 mb-5 button-date-session">
-        <NavLink to="/user/onehour">
-          <ButtonForMe childrenButton={"Next"} />
-        </NavLink>
+        <ButtonForMe childrenButton={"Next"} onclick={handleSubmitDate} />
       </div>
       <div className="legal-notice-user">
         <LegalNotice />
--- src/components/home/wordFindCare/userSignIn/UserSignIn.jsx
+++ src/components/home/wordFindCare/userSignIn/UserSignIn.jsx
@@ -5,6 +5,7 @@ import "./UserSignIn.css";
 
 import { FormSignIn } from "../../formSignIn/FormSignIn";
 import { LegalNotice } from "../../../carehub/LegalNotice";
+import UserServiceAPI from "../../../../service/userServiceAPI";
 
 export function UserSignIn() {
   return (
@@ -17,8 +18,8 @@ export function UserSignIn() {
           <div className="col-2" style={{ backgroundColor: "#3b71aa", height: "3px" }}></div>
         </div>
       </div>
-      <div style={{margin:'5% 0'}}>
-      <FormSignIn url={"/user/address"} />
+      <div style={{ margin: "5% 0" }}>
+        <FormSignIn url={"/user/address"} checkRole={"ROLE_USER"} api ={UserServiceAPI.signInUser} />
       </div>
       <div className="legal-notice-user">
         <LegalNotice />
