8282f62a2556674b4882cfbf98e4bfc9d27224aa
ANHTUANPHUNG <132957644+ANHTUANPHUNG@users.noreply.github.com>
2024-01-24 11:13:07 +0700

    tuandev 23/1

--- src/components/home/wordFindCare/datesession/DateBetween.jsx
+++ src/components/home/wordFindCare/datesession/DateBetween.jsx
@@ -3,8 +3,6 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers-pro';
 import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-import dayjs from 'dayjs';
-import { toast } from 'react-toastify';
 
 export default function DateBetween({setSelectedDate,selectedDate}) {
 
--- src/components/viewUser/contract/Contract.jsx
+++ src/components/viewUser/contract/Contract.jsx
@@ -3,14 +3,49 @@ import "./Contract.css";
 import { LegalNotice } from "../../carehub/LegalNotice";
 import { ContainerViewUser } from "../containerViewUser/ContainerViewUser";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
-export function Contract(){
-    const {id} = useParams()
-    return(
-        <>
-                  <ContainerViewUser idUser={id} />
-                  <LegalNotice />
-
-        </>
-    )
+export function Contract() {
+  const { id } = useParams();
+  const [listContract, setListContract] = useState([]);
+  useEffect(() => {
+    axios.get(`http://localhost:8080/api/contracts/users/${id}`).then((res) => {
+      const arrayList = res.data.content.map((e, i) => ({
+        ...e,
+        indexContract: i + 1,
+      }));
+      setListContract(arrayList);
+    });
+  }, [id]);
+  return (
+    <>
+      <ContainerViewUser idUser={id} />
+      <div style={{ padding: "30px 70px" }}>
+        <h2>Danh sách hợp đồng</h2>
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Thời gian ký </th>
+              <th>Hiệu lực</th>
+              <th>Tên khách hàng</th>
+              <th>Tên hộ lý</th>
+              <th>Hoạt động</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listContract?.map((e) => (
+              <tr key={e.id}>
+                <td>{e.indexContract}</td>
+                <td>{e.createAt}</td>
+                <td>{e.timeStart} {" "} {e.timeEnd}</td>
+                <td></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <LegalNotice />
+    </>
+  );
 }
